ac7e30ca9814d0ab235c8b66c1ed0f5aecc7c016
kimevm <122947330+kimevm@users.noreply.github.com>
2025-02-11 13:54:34 -0500

    Create pa2.c

---
+++ pa2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/* Constants */
+#define SAMPLES 10000
+#define RUNS 50000
+#define BINS 64
+#define HISTOGRAM_SPAN 0.05
+#define SCALE 32
+
+
