389b770cc09d67cb3dd240cd587a90c7644fa118
Nicholas Hernandez <112581921+nhern026@users.noreply.github.com>
2025-04-27 01:29:44 -0400

    Updated Files for PA7

--- pa7/mem_alloc.c
+++ pa7/mem_alloc.c
@@ -4,8 +4,8 @@
  * Author: Lawrence Kim - kimevm@bc.edu, Nicholas Hernandez - hernantx@bc.edu
  */
 
-#include <sys/mman.h>
 #include <stdio.h>
+#include <sys/mman.h>
 
 #include "mem_alloc.h"
 
@@ -18,39 +18,32 @@ void set_allocated(Header *h);
 void set_free(Header *h);
 Header *get_header(void *mem);
 int same_page(Header *h1, Header *h2);
+int mem_init(void);
+int mem_extend(Header *last);
 
-static int mem_init(void);
-static int mem_extend(Header *last);
-
-
-
-static int mem_init(void) {
-    void *page = mmap(NULL, PAGE_SIZE, PROT_REAED | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+int mem_init(void) {
+    void *page = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (page == MAP_FAILED) {
         return FAILURE;
     }
-    
     free_list = (Header *)page;
     free_list->size = PAGE_SIZE - sizeof(Header);
     free_list->next = NULL;
-    free_list->prev = NULL;
+    free_list->previous = NULL;
     set_free(free_list);
-
     return SUCCESS;
 }
 
-static int mem_extend(Header * last) {
-    void *page = mmap(NULL, PAGE_SIZE, PROT_REAED | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+int mem_extend(Header * last) {
+    void *page = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (page == MAP_FAILED) {
         return FAILURE;
     }
-
     Header *h = (Header *)page;
     h->size = PAGE_SIZE - sizeof(Header);
     h->next = NULL;
-    h->prev = last;
+    h->previous= last;
     set_free(h);
-
     last->next = h;
     return SUCCESS;
 }
@@ -59,20 +52,30 @@ void * mem_alloc(size_t requested_size) {
     if (requested_size > PAGE_SIZE - sizeof(Header)) {
         return NULL;
     }
-
     size_t aligned = ((requested_size + WORD_SIZE - 1) / WORD_SIZE) * WORD_SIZE;
-
-    if (free_list == NULL) {
-        if (mem_init() == FAILURE) {
-            return NULL;
-        }
-    }
-
-    if (is_allocated(h) || get_size(h) < aligned) {
-        if (mem_extend(h) == FAILURE) {
-            return NULL;
+    Header *h = free_list;
+    while (h != NULL) {
+        if (is_free(h) && get_size(h) >= aligned) {
+            break; 
         }
         h = h->next;
+    }
+    if (h == NULL) {
+        if (free_list == NULL) {
+            if (mem_init() == FAILURE) {
+                return NULL;
+            }
+            h = free_list;
+        } else {
+            Header *last = free_list;
+            while (last->next != NULL) {
+                last = last->next;
+            }
+            if (mem_extend(last) == FAILURE) {
+                return NULL;
+            }
+            h = last->next; 
+        }
     }
 
     size_t total_size = get_size(h);
@@ -80,13 +83,12 @@ void * mem_alloc(size_t requested_size) {
         Header * new_hdr = (Header *)((char *)h + sizeof(Header) + aligned);
         new_hdr->size = total_size - aligned - sizeof(Header);
         new_hdr->next = h->next;
-        new_hdr->prev = h;
+        new_hdr->previous = h;
         if (h->next) {
-            h->next->prev = new_hdr;
+            h->next->previous = new_hdr;
         }
         h->next = new_hdr;
         set_free(new_hdr);
-
         h->size = aligned;
     }
 
@@ -98,62 +100,64 @@ void mem_free(void * ptr) {
     if (ptr == NULL) {
         return;
     }
-
     Header * h = get_header(ptr);
     set_free(h);
-
     Header * n = h->next;
     if (n && is_free(n) && same_page(h,n)) {
         h->size = get_size(h) + sizeof(Header) + get_size(n);
         h->next = n->next;
         if (n->next) {
-            n->next->prev = h;
+            n->next->previous = h;
         }
     }
 
-    Header * p = h->prev;
+    Header * p = h->previous;
     if (p && is_free(p) && same_page(p, h)) {
         p->size = get_size(p) + sizeof(Header) + get_size(h);
         p->next = h->next;
         if(h->next) {
-            h->next->prev = p;
+            h->next->previous = p;
         }
         h = p;
     }
-
     if (get_size(h) == PAGE_SIZE - sizeof(Header)) {
-        Header * prev = h->prev;
+        Header * previous = h->previous;
         Header * next = h->next;
-
-        if (prev) {
-            prev->next = next;
+        if (previous) {
+            previous->next = next;
         }
         if (next) {
-            next->prev = prev;
+            next->previous = previous;
         }
         if (h == free_list) {
             free_list = next;
         }
-
         munmap((void *)h, PAGE_SIZE);
-
-        if (prev == NULL && next == NULL) {
+        if (previous == NULL && next == NULL) {
             free_list = NULL;
         }
     }
 }
 
-/*Print functions need work(Delete this comment later)*/
-
-void print_header(Header * h) {
+void print_header(Header * h) { 
+    printf("Addr: %p\n", (void *)h);
+    printf("    Size: %zu\n", get_size(h));
+    printf("    Previous: %p\n", (void *)h->previous);
+    printf("    Next: %p\n", (void *)h->next);
 }
-
 
 void print_list(void) {
+    if (free_list == NULL) {
+        puts("(Empty list.)");
+        return;
+    }
+    Header *curr = free_list;
+    while (curr != NULL) {
+        printf("%p -> ", (void *)curr);
+        curr = curr->next;
+    }
+    printf("\n");
 }
-
-
-/* Helper Implementation (Delete this comment later)*/
 
 int is_allocated(Header *h) {
     return (h->size & 1);
--- pa7/mem_alloc.h
+++ pa7/mem_alloc.h
@@ -2,24 +2,21 @@
  * Author: Lawrence Kim - kimevm@bc.edu, Nicholas Hernandez - hernantx@bc.edu
  */
 
+#include <stddef.h>
+
+#define FAILURE -1
 #ifndef MEM_ALLOC_H
 #define MEM_ALLOC_H
-
-#include <stddef.h>
 #define PAGE_SIZE 4096
-#define WORD_SIZE 8
 #define SUCCESS 0
-#define FAILURE -1
+#define WORD_SIZE 8
 
 typedef struct Header {
     size_t        size;
     struct Header * next;
-    struct Header * prev;
+    struct Header * previous;
 } Header;
-
 extern Header * free_list;
-
 void * mem_alloc(size_t requested_size);
 void mem_free(void * ptr);
-
 #endif
